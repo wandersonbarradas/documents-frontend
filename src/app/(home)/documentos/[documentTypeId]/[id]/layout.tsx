02f5f8bb8a485ab8da810ba5c664a2931b9d5ef9
Wanderson Barradas <wandersonbarradas07@gmail.com>
2024-02-20 21:19:29 -0300

    add type metadata

--- src/app/(home)/documentos/[documentTypeId]/[id]/layout.tsx
+++ src/app/(home)/documentos/[documentTypeId]/[id]/layout.tsx
@@ -1,9 +1,14 @@
+import { Metadata } from "next";
+
 type Props = {
     params: { id: string };
     searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export const generateMetadata = async ({ params, searchParams }: Props) => {
+export const generateMetadata = async ({
+    params,
+    searchParams,
+}: Props): Promise<Metadata> => {
     if (params.id === "novo") {
         return {
             title: "Novo documento",
